152eec0928c81fe5df8401f7f651b80cef10198a
ramseywise <ramsey.wise@gmail.com>
2023-07-03 14:02:17 +0200

    add comments

--- src/lessons/1__Making the Zombie Factory/newzombie.sol
+++ src/lessons/1__Making the Zombie Factory/newzombie.sol
@@ -1,9 +1,9 @@
 pragma solidity >=0.5.0 <0.6.0;
 
 contract ZombieFactory {
+    // Declare contract event with defined parameters and their structure.
 
     event NewZombie(uint zombieId, string name, uint dna);
-
     uint dnaDigits = 16;
     uint dnaModulus = 10 ** dnaDigits;
 
@@ -15,18 +15,21 @@ contract ZombieFactory {
     Zombie[] public zombies;
 
     function _createZombie(string memory _name, uint _dna) private {
+        // Removes zombie from factory when new one is introduced.
         uint id = zombies.push(Zombie(_name, _dna)) - 1;
         emit NewZombie(id, _name, _dna);
     }
 
     function _generateRandomDna(string memory _str) private view returns (uint) {
+        // Generate new zombie dna using random has generator.
         uint rand = uint(keccak256(abi.encodePacked(_str)));
         return rand % dnaModulus;
     }
 
     function createRandomZombie(string memory _name) public {
+        // Returns new zombie.
         uint randDna = _generateRandomDna(_name);
         _createZombie(_name, randDna);
     }
-
+    
 }
