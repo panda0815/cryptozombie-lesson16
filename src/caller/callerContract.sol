b9c92b1b63cd830c6b07915e28d4d360aefeafd4
ramseywise <ramsey.wise@gmail.com>
2023-08-03 15:16:35 +0200

    add oracle caller files

---
+++ src/caller/callerContract.sol
@@ -0,0 +1,35 @@
+pragma solidity 0.5.0;
+import "./EthPriceOracleInterface.sol";
+import "openzeppelin-solidity/contracts/ownership/Ownable.sol";
+
+contract CallerContract is Ownbable {
+    uint256 private ethPrice; // declare price
+    EthPriceOracleInterface private oracleInstance; // declare oracle instance
+    address private oracleAddress; // provide address of oracle smart contract
+    
+    event newOracleAddressEvent(address oracleAddress);
+    event ReceivedNewRequestIdEvent(uint256 id);
+    event PriceUpdatedEvent(uint256 ethPrice, uint256 id);
+
+    function setOracleInstanceAddress (address _oracleInstanceAddress) public onlyOwner {
+        oracleAddress = _oracleInstanceAddress; // set oracle address
+        oracleInstance = EthPriceOracleInterface(oracleAddress); // instantiate and store results
+        emit newOracleAddressEvent(oracleAddress); // fire event
+    }
+    function updateEthPrice() public {
+        uint256 id = oracleInstance.getLatestEthPrice();
+        myRequests[id] = true;
+        emit ReceivedNewRequestIdEvent(id);
+    }
+    function callback(uint256 _ethPrice, uint256 _id) public {
+        require(myRequests[_id], "This request is not in my pending list.");
+        ethPrice = _ethPrice;
+        delete myRequests[_id];
+        emit PriceUpdatedEvent(_ethPrice, _id);
+    }
+
+    modifier onlyOracle() {
+      require(msg.sender == oracleAddress, "You are not authorized to call this function.");
+      _;
+    }
+}
---
+++ src/test/testZombieFactory.js
@@ -0,0 +1,38 @@
+//import truffle assertions
+const truffleAssert = require('truffle-assertions')
+
+// import the contract artifact
+const ZombieFactory = artifacts.require('./ZombieFactory.sol')
+
+// Test starts here
+contract('ZombieFactory', function (accounts) {
+  // predefine the contract instance
+  let ZombieFactoryInstance
+
+  // before each test, create a new contract instance
+  beforeEach(async function () {
+    ZombieFactoryInstance = await ZombieFactory.new()
+
+  })
+
+
+  // Tests for creatRandomZombie function 
+  it('should only create one zombie per address', async function () {
+    await ZombieFactoryInstance.createRandomZombie("Mike", { 'from': accounts[0] })
+    let ownerZombieCounts = await ZombieFactoryInstance.ownerZombieCount(accounts[0])
+    assert.equal(ownerZombieCounts, 1, 'One zombie was not created for address 0 ')
+
+  })
+
+  // Test for creatRandomZombie function 
+  it('should not be able to create a second zombie per address', async function () {
+    await ZombieFactoryInstance.createRandomZombie("Mike", { 'from': accounts[0] })
+    await truffleAssert.reverts(ZombieFactoryInstance.createRandomZombie("Mike", { 'from': accounts[0] }))
+    // TypeError: Cannot read property "0x62730..."
+    let ownerZombieCounts = await ZombieFactoryInstance.ownerZombieCount(accounts[0])
+    // error says ZombieFactoryInstance.ownerZombieCount is not a function
+    assert.equal(ownerZombieCounts, 1, 'Second zombie was created for address 0 ')
+  })
+
+
+})
